7d975b37b2132e35d1dda5a753c2794c81ef5903
justin1492 <justin1492@126.com>
2017-11-21 20:49:05 +0800

    no message

--- src/scene/Mine/MineScene.js
+++ src/scene/Mine/MineScene.js
@@ -20,6 +20,23 @@ class MineScene extends Component {
       headerTintColor: color.title,
       headerStyle: { backgroundColor: color.theme },
   })
+  state: {
+    isRefreshing:boolean
+  }
+  constructor(props: Object) {
+      super(props)
+
+      this.state = {
+          isRefreshing: false
+      }
+  }
+
+  onHeadRefresh(){
+    this.setState({isRefreshing: true})
+    setTimeout(()=>{
+      this.setState({isRefreshing: false})
+    },2000);
+  }
   renderHeader(){
     return(
         <View style = {styles.header}>
@@ -31,12 +48,73 @@ class MineScene extends Component {
 
         </View>
 
+    )
+  }
+  renderCells() {
+    let cells = []
+    let dataList = this.getData()
+    for (let i = 0; i < dataList.length; i++) {
+      let sublist = dataList[i]
+      for (let j = 0; j < sublist.length; j++) {
+        let data = sublist[j]
+        let cell = <DetailCell image={data.image} title={data.title} subtitle={data.subtitle} key={data.title} />
+        cells.push(cell)
+      }
+      cells.push(<SpacingView key={i} />)
+    }
+    return (
+        <View style={{ flex: 1 }}>
+            {cells}
+        </View>
     );
   }
+  getData(){
+    return (
+      [
+        [
+          {title: '蚂蚁会员', subtitle: '提现免提现服务费', image: require('../../img/Mine/icon_mine_wallet.png')},
+          {title: '商家服务', image: require('../../img/Mine/icon_mine_wallet.png')},
+        ],
+        [
+          {title: '账单', image: require('../../img/Mine/icon_mine_wallet.png')},
+          {title: '总资产', subtitle: '账户安全保护中', image: require('../../img/Mine/icon_mine_wallet.png')},
+          {title: '余额', subtitle: '0.00元', image: require('../../img/Mine/icon_mine_wallet.png')},
+          {title: '余额宝', image: require('../../img/Mine/icon_mine_wallet.png')},
+          {title: '余利宝', image: require('../../img/Mine/icon_mine_friends.png')},
+          {title: '银行卡', image: require('../../img/Mine/icon_mine_friends.png')},
+        ],
+        [
+          {title: '蚂蚁财富', image: require('../../img/Mine/icon_mine_comment.png')},
+          {title: '芝麻信用', image: require('../../img/Mine/icon_mine_comment.png')},
+          {title: '保险服务', image: require('../../img/Mine/icon_mine_comment.png')},
+          {title: '花呗', image: require('../../img/Mine/icon_mine_comment.png')},
+          {title: '网商贷', image: require('../../img/Mine/icon_mine_comment.png')},
+          {title: '网上银行', image: require('../../img/Mine/icon_mine_comment.png')},
+
+        ],
+        [
+          {title: '爱心捐赠', image: require('../../img/Mine/icon_mine_customerService.png')},
+          {title: '娱乐宝', image: require('../../img/Mine/icon_mine_customerService.png')},
+        ]
+      ]
+
+    )
+  }
+
   render(){
       return (
           <View style={styles.container}>
-                {this.renderHeader()}
+              <ScrollView
+                  refreshControl={
+                    <RefreshControl
+                        refreshing={this.state.isRefreshing}
+                        onRefresh={() => this.onHeadRefresh()}
+                        tintColor='gray'
+                    />
+                  }>
+                  {this.renderHeader()}
+                  {this.renderCells()}
+              </ScrollView>
           </View>
       );
   }
